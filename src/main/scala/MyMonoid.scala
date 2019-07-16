50817dc5a5b79a55cdbb2a916dfd3098b2a7df61
nosoosso <nosoosso.null@gmail.com>
2019-07-16 11:14:42 +0900

    Add Monoid example

---
+++ src/main/scala/MyMonoid.scala
@@ -0,0 +1,35 @@
+object MyMonoid {
+  def main(args: Array[String]): Unit = {
+    println(1 combine 2)
+    println(sum(List(1,2,3,4,5)))
+    println(unsafeSum(List(1,2,3,4,5)))
+    println(unsafeSum(List[Int]()))
+  }
+}
+
+trait SemiGroup[T] {
+  def (x: T) combine (y: T): T
+}
+
+trait Monoid[T] extends SemiGroup[T] {
+  def unit: T
+}
+
+object SemiGroup {
+  def apply[T] given SemiGroup[T] = the[SemiGroup[T]]
+}
+
+object Monoid {
+  def apply[T] given Monoid[T] = the[Monoid[T]]
+}
+
+delegate for Monoid[Int] {
+  def (x: Int) combine (y: Int): Int = x + y
+  def unit: Int = 0
+}
+
+def unsafeSum[T: SemiGroup](xs: List[T]): T =
+  xs.reduceLeft(_.combine(_))
+
+def sum[T: Monoid](xs: List[T]): T =
+  xs.foldLeft(Monoid[T].unit)(_.combine(_))
